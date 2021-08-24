de87772cd8920d2f50c460402dbdb17c9fcc6892
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-08-24 17:01:55 -0300

    fim da atividade

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Fogo");
+        }
+    }
+}
